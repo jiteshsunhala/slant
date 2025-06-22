10addfaaf51715def4a5457cc52315d6b13e4fa0
Jitesh Sunhala <jiteshsunhala@Jiteshs-MacBook-Air.local>
2025-06-21 22:14:15 -0400

    adding diagonal two pair

---
+++ src/main/java/com/sunhaj/slant/steps/pairs/twoPair/DiagonalTwoPairStep.java
@@ -0,0 +1,136 @@
+package com.sunhaj.slant.steps.pairs.twoPair;
+
+import com.sunhaj.slant.model.Corner;
+import com.sunhaj.slant.steps.pairs.PairCondition;
+import com.sunhaj.slant.steps.pairs.PairStep;
+import com.sunhaj.slant.util.EdgedCorner;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DiagonalTwoPairStep extends PairStep {
+
+    @Override
+    protected List<PairCondition> getPairConditions() {
+        return List.of(
+                getPairConditionBuilder()
+                        .pairDirection(Corner.Direction.right)
+                        .startCellConditions(List.of(
+                                new PairCondition.CellCondition(
+                                        Corner::getBottomLeft,
+                                        (board, cell) -> cell.getIncoming().equals(board.getCellValue(cell)),
+                                        Corner.Cell::getIncoming
+                                ),
+                                new PairCondition.CellCondition(
+                                        Corner::getTopLeft,
+                                        (board, cell) -> !cell.getIncoming().equals(board.getCellValue(cell)),
+                                        Corner.Cell::getAway
+                                )
+                        ))
+                        .endCellConditions(List.of(
+                                new PairCondition.CellCondition(
+                                        Corner::getTopRight,
+                                        (board, cell) -> cell.getIncoming().equals(board.getCellValue(cell)),
+                                        Corner.Cell::getIncoming
+                                ),
+                                new PairCondition.CellCondition(
+                                        Corner::getBottomRight,
+                                        (board, cell) -> !cell.getIncoming().equals(board.getCellValue(cell)),
+                                        Corner.Cell::getAway
+                                )
+                        ))
+                        .build(),
+                getPairConditionBuilder()
+                        .pairDirection(Corner.Direction.right)
+                        .startCellConditions(List.of(
+                                new PairCondition.CellCondition(
+                                        Corner::getTopLeft,
+                                        (board, cell) -> cell.getIncoming().equals(board.getCellValue(cell)),
+                                        Corner.Cell::getIncoming
+                                ),
+                                new PairCondition.CellCondition(
+                                        Corner::getBottomLeft,
+                                        (board, cell) -> !cell.getIncoming().equals(board.getCellValue(cell)),
+                                        Corner.Cell::getAway
+                                )
+                        ))
+                        .endCellConditions(List.of(
+                                new PairCondition.CellCondition(
+                                        Corner::getBottomRight,
+                                        (board, cell) -> cell.getIncoming().equals(board.getCellValue(cell)),
+                                        Corner.Cell::getIncoming
+                                ),
+                                new PairCondition.CellCondition(
+                                        Corner::getTopRight,
+                                        (board, cell) -> !cell.getIncoming().equals(board.getCellValue(cell)),
+                                        Corner.Cell::getAway
+                                )
+                        ))
+                        .build(),
+                getPairConditionBuilder()
+                        .pairDirection(Corner.Direction.bottom)
+                        .startCellConditions(List.of(
+                                new PairCondition.CellCondition(
+                                        Corner::getTopLeft,
+                                        (board, cell) -> cell.getIncoming().equals(board.getCellValue(cell)),
+                                        Corner.Cell::getIncoming
+                                ),
+                                new PairCondition.CellCondition(
+                                        Corner::getTopRight,
+                                        (board, cell) -> !cell.getIncoming().equals(board.getCellValue(cell)),
+                                        Corner.Cell::getAway
+                                )
+                        ))
+                        .endCellConditions(List.of(
+                                new PairCondition.CellCondition(
+                                        Corner::getBottomRight,
+                                        (board, cell) -> cell.getIncoming().equals(board.getCellValue(cell)),
+                                        Corner.Cell::getIncoming
+                                ),
+                                new PairCondition.CellCondition(
+                                        Corner::getBottomLeft,
+                                        (board, cell) -> !cell.getIncoming().equals(board.getCellValue(cell)),
+                                        Corner.Cell::getAway
+                                )
+                        ))
+                        .build(),
+                getPairConditionBuilder()
+                        .pairDirection(Corner.Direction.bottom)
+                        .startCellConditions(List.of(
+                                new PairCondition.CellCondition(
+                                        Corner::getTopRight,
+                                        (board, cell) -> cell.getIncoming().equals(board.getCellValue(cell)),
+                                        Corner.Cell::getIncoming
+                                ),
+                                new PairCondition.CellCondition(
+                                        Corner::getTopLeft,
+                                        (board, cell) -> !cell.getIncoming().equals(board.getCellValue(cell)),
+                                        Corner.Cell::getAway
+                                )
+                        ))
+                        .endCellConditions(List.of(
+                                new PairCondition.CellCondition(
+                                        Corner::getBottomLeft,
+                                        (board, cell) -> cell.getIncoming().equals(board.getCellValue(cell)),
+                                        Corner.Cell::getIncoming
+                                ),
+                                new PairCondition.CellCondition(
+                                        Corner::getBottomRight,
+                                        (board, cell) -> !cell.getIncoming().equals(board.getCellValue(cell)),
+                                        Corner.Cell::getAway
+                                )
+                        ))
+                        .build()
+        );
+    }
+
+    private PairCondition.PairConditionBuilder getPairConditionBuilder() {
+        return PairCondition.builder()
+                .skipValues(List.of(2))
+                .startCornerValue(2)
+                .startCornerCondition(new EdgedCorner().negate())
+                .endCornerValue(2)
+                .endCornerCondition(new EdgedCorner().negate());
+    }
+}
